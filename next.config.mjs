0419bc55b20f97222f3d02618b4761391dd24c29
oagree0123 <oagree@nvaer.com>
2025-04-13 19:22:05 +0900

    chore: 프로젝트 초기 설정
    - Next.js 14.2.25 설정
    - Vanilla Extract 설치
    - ESLint, Prettier 설정 추가

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+
+const withVanillaExtract = createVanillaExtractPlugin();
+
+export default withVanillaExtract({
+  reactStrictMode: true,
+});
